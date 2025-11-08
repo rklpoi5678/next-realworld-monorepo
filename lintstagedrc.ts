9eed1299ca3da45a5dda0eb9ddd6dbc94926558e
rklpoi5678 <rklpoi5678@naver.com>
2025-11-08 18:49:42 +0900

    chore: 공용 tsconfig 설정

--- lintstagedrc.ts
+++
@@ -1,8 +0,0 @@
-import path from 'path'
-
-const buildEslintCommand = (filenames: string[]) =>
-  `eslint --fix ${filenames.map((f: string) => `"${path.relative(process.cwd(), f)}"`).join(' ')}`
-
-module.exports = {
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
-}
