91438f2715327d9986572d0b94b2cc23b295c9d5
rklpoi5678 <rklpoi5678@naver.com>
2025-12-29 10:13:38 +0900

    Chore: 린트 스테이지 테스트

--- apps/bulletproof-next-16/lint-staged.config.mjs
+++ apps/bulletproof-next-16/lint-staged.config.mjs
@@ -10,7 +10,7 @@ const config = {
     // ts/tsx: lint & formatting
   '*.${js,jsx,ts,tsx}': [buildEslintCommand,'prettier --write'],
   // 타입 체크: 변경사항이 있을 때 프로젝트 전체 검사
-  '**/*.{ts.tsx}': [() => 'pnpm check-types']
+  '**/*.{ts,tsx}': [() => 'pnpm check-types']
 };
 
 export default config;
