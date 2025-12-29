2a322f05a9891057365e5b976889423ba0a96a8c
rklpoi5678 <rklpoi5678@naver.com>
2025-12-29 15:24:01 +0900

    Feat: (msw) discussions 핸들러 추가

--- apps/bulletproof-next-16/src/testing/mocks/handlers/index.ts
+++ apps/bulletproof-next-16/src/testing/mocks/handlers/index.ts
@@ -5,11 +5,13 @@ import { env } from '@/config/env';
 import { networkDelay } from '../utils';
 import { authHandlers } from './auth';
 import { commentsHandlers } from './comments';
+import { discussionsHandlers } from './discussions';
 
 /** 모든 도메인별 핸들러 통합 */
 export const handlers = [
-    ...authHandlers,
-    ...commentsHandlers,
+  ...authHandlers,
+  ...commentsHandlers,
+  ...discussionsHandlers,
   /** 공통 / 유틸리티 핸들러 */
   http.get(`${env.API_URL}/healthcheck`, async () => {
     //인위적인 네트워크 지연 추가 (개발 환경에서 체감을 위해)
