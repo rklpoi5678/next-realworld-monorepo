7024714e81e622e328e9ed38d8124d4cf63cf1f3
rklpoi5678 <rklpoi5678@naver.com>
2025-12-29 14:41:40 +0900

    Feat: (msw) auth핸들러 추가

--- apps/bulletproof-next-16/src/testing/mocks/handlers/index.ts
+++ apps/bulletproof-next-16/src/testing/mocks/handlers/index.ts
@@ -2,11 +2,16 @@ import { HttpResponse, http } from 'msw';
 
 import { env } from '@/config/env';
 
+import { networkDelay } from '../utils';
+import { authHandlers } from './auth';
+
 /** 모든 도메인별 핸들러 통합 */
 export const handlers = [
+    ...authHandlers,
   /** 공통 / 유틸리티 핸들러 */
   http.get(`${env.API_URL}/healthcheck`, async () => {
     //인위적인 네트워크 지연 추가 (개발 환경에서 체감을 위해)
+    await networkDelay();
 
     return HttpResponse.json(
       {
