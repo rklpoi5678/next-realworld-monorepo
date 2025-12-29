379245be927ef050d75009e0755471642280eb90
rklpoi5678 <rklpoi5678@naver.com>
2025-12-29 10:05:44 +0900

    Chore: husky 설정 변경

--- apps/bulletproof-next-16/lint-staged.config.mjs
+++ apps/bulletproof-next-16/lint-staged.config.mjs
@@ -1,14 +1,22 @@
 import path from 'path';
 
+/** EsLint 커맨드 빌더 */
 const buildEslintCommand = (filenames) => {
-  return `next lint --fix --file ${filenames
-    .filter((f) => f.includes('/src'))
-    .map((f) => path.relative(process.cwd(), f))
-    .join('--file')}`;
+    // 현재 경로 기준으로 상대 경로 추출
+    const relativeFiles = filenames.map((f) => path.relative(process.cwd(), f))
+    .join('--file');
+  return `next lint --fix --file ${relativeFiles}`
 };
 
 const config = {
-  '*.${ts,tsx}': [buildEslintCommand, "bash -c 'pnpm check-types"],
+    // ts/tsx: lint & formatting
+  '*.${ts,tsx}': [
+  buildEslintCommand,'prettier --write'
+  ],
+  // 타입 체크: 변경사항이 있을 때 프로젝트 전체 검사
+  '**/*.{ts.tsx}': [
+    () => 'pnpm check-types'
+  ]
 };
 
 export default config;
