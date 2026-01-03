01ada3d84c0027413022c31b634a9e9f2830b316
rklpoi5678 <rklpoi5678@naver.com>
2026-01-03 23:43:26 +0900

    Feat: delete-discussion 컴포넌트 추가

---
+++ apps/bulletproof-next-16/src/features/discussions/components/delete-discussion.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import { Trash } from 'lucide-react';
+
+import { Button } from '@/components/ui/button';
+import { ConfirmationDialog } from '@/components/ui/dialog';
+import { useNotifications } from '@/components/ui/notifications';
+import { useUser } from '@/libs/auth';
+import { policies } from '@/libs/authorization';
+
+import { useDeleteDiscussion } from '../api/delete-discussion';
+
+
+type DeleteDiscussionProps = {
+  id: string;
+};
+
+export const DeleteDiscussion = ({ id }: DeleteDiscussionProps) => {
+  const user = useUser();
+  const { addNotification } = useNotifications();
+  const deleteDiscussionMutation = useDeleteDiscussion({
+    mutationConfig: {
+      onSuccess: () => {
+        addNotification({
+          type: 'success',
+          title: 'Discussion Deleted',
+        });
+      },
+    },
+  });
+
+  if (!policies['discussion:delete'](user?.data)) {
+    return null;
+  }
+
+  return (
+    <ConfirmationDialog
+      icon="danger"
+      title="Delete Discussion"
+      body="Are you sure you want to delete this discussion?"
+      triggerButton={
+        <Button variant="destructive" icon={<Trash className="size-4" />}>
+          Delete Discussion
+        </Button>
+      }
+      confirmButton={
+        <Button
+          isLoading={deleteDiscussionMutation.isPending}
+          type="button"
+          variant="destructive"
+          onClick={() => deleteDiscussionMutation.mutate({ discussionId: id })}
+        >
+          Delete Discussion
+        </Button>
+      }
+    />
+  );
+};
