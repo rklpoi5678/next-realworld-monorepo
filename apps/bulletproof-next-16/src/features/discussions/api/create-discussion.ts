5886513527474c898359e7d0f42a73e41926fe03
rklpoi5678 <rklpoi5678@naver.com>
2026-01-03 22:53:51 +0900

    Feat: create-discussion 생성

---
+++ apps/bulletproof-next-16/src/features/discussions/api/create-discussion.ts
@@ -0,0 +1,46 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { z } from 'zod';
+
+import { api } from '@/libs/api-client';
+import { MutationConfig } from '@/libs/react-query';
+import { Discussion } from '@/types/api';
+
+import { getDiscussionsQueryOptions } from './get-discussions';
+
+export const createDiscussionInputSchema = z.object({
+  title: z.string().trim().min(1, 'Title is required'),
+  body: z.string().trim().min(1, 'Body is required'),
+  public: z.boolean().default(false),
+});
+
+export type createDiscussionInput = z.infer<typeof createDiscussionInputSchema>;
+
+export const createDiscussion = ({
+  data,
+}: {
+  data: createDiscussionInput;
+}): Promise<Discussion> => {
+  return api.post('/discussions', data);
+};
+
+type UseCreateDiscussionOptions = {
+  mutationConfig?: MutationConfig<typeof createDiscussion>;
+};
+
+export const useCreateDiscussion = ({ mutationConfig }: UseCreateDiscussionOptions = {}) => {
+  const queryClient = useQueryClient();
+  const { onSuccess, ...restConfig } = mutationConfig || {};
+
+  return useMutation({
+    ...restConfig,
+    mutationFn: createDiscussion,
+    onSuccess: (...args) => {
+      // invalidate: Discussions list를 최신 상태로 갱신
+      queryClient.invalidateQueries({
+        queryKey: getDiscussionsQueryOptions().queryKey,
+      });
+      // User가 전달한 추가 onSuccess Callback 실행
+      onSuccess?.(...args);
+    },
+  });
+};
