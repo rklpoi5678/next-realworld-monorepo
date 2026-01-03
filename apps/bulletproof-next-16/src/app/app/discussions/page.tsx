5a8ebfc2b4d1e73e1aff9f91d22466446cabfdcd
rklpoi5678 <rklpoi5678@naver.com>
2026-01-04 00:27:15 +0900

    Feat: 서버사이드  하이드레이션 논의페이지

---
+++ apps/bulletproof-next-16/src/app/app/discussions/page.tsx
@@ -0,0 +1,38 @@
+import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
+import { Metadata } from 'next';
+
+import { getDiscussionsQueryOptions } from '@/features/discussions/api/get-discussions';
+
+import { Discussions } from './_components/discussions';
+
+export const metadata: Metadata = {
+  title: 'Discussion',
+  description: 'View and manage community discussions',
+};
+
+export default async function DiscussionPage({
+  searchParams,
+}: {
+  searchParams: { page: string | null };
+}) {
+  const resolvedSearchParams = await searchParams;
+  const page = typeof resolvedSearchParams.page === 'string' ? Number(resolvedSearchParams) : 1;
+
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 1000 * 60, //서버에서 패치한 데이터가 즉시 stale해지는 것을 방지
+      },
+    },
+  });
+
+  await queryClient.prefetchQuery(getDiscussionsQueryOptions({ page }));
+
+  const dehydratedState = dehydrate(queryClient);
+
+  return (
+    <HydrationBoundary state={dehydratedState}>
+      <Discussions />
+    </HydrationBoundary>
+  );
+}
