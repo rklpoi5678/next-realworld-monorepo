81c32021aa42ae44fe169c213d171c8231eea99a
rklpoi5678 <rklpoi5678@naver.com>
2026-01-03 22:50:35 +0900

    Feat: get-discussion api함수 추가

---
+++ apps/bulletproof-next-16/src/features/discussions/api/get-discussions.ts
@@ -0,0 +1,34 @@
+import { keepPreviousData, queryOptions, useQuery } from '@tanstack/react-query';
+
+import { api } from '@/libs/api-client';
+import { QueryConfig } from '@/libs/react-query';
+
+/** API  Func */
+export const getDiscussions = ({ page = 1 }: { page?: number } = {}) => {
+  return api.get('/discussions', {
+    params: { page },
+  });
+};
+
+/** Query Options */
+export const getDiscussionsQueryOptions = ({ page = 1 }: { page?: number } = {}) => {
+  return queryOptions({
+    queryKey: ['discussion', { page }],
+    queryFn: () => getDiscussions({ page }),
+  });
+};
+
+type UseDiscussionsOptions = {
+  page?: number;
+  queryConfig?: QueryConfig<typeof getDiscussionsQueryOptions>;
+};
+
+// useDiscussions인자 자체에 = {} 기본값을 주어 옵션없이 호출시에도 에러가 나지 않도록 안정성을 높힌다.
+export const useDiscussions = ({ page = 1, queryConfig }: UseDiscussionsOptions = {}) => {
+  return useQuery({
+    ...getDiscussionsQueryOptions({ page }),
+    ...queryConfig,
+    // 페이지네이션 시 다음 페이지 로드 전까지 이전 데이터를 보여주어 UX개선
+    placeholderData: keepPreviousData,
+  });
+};
