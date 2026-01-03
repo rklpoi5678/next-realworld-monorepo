1073610e178f0d405e2ac828ebffa97b744dc929
rklpoi5678 <rklpoi5678@naver.com>
2026-01-03 18:04:44 +0900

    Fix: 모노레포 자식 lint-staged설정 변경 next lint대신 eslint --ix

--- apps/bulletproof-next-16/lint-staged.config.mjs
+++ apps/bulletproof-next-16/lint-staged.config.mjs
@@ -2,15 +2,13 @@ import path from 'path';
 
 /** EsLint 커맨드 빌더 */
 const buildEslintCommand = (filenames) =>
-  `next lint --fix --file ${filenames
-    .map((f) => path.relative(process.cwd(), f))
-    .join(' --file ')}`;
+  `eslint --fix  ${filenames.map((f) => path.relative(process.cwd(), f)).join(' ')}`;
 
 const config = {
-    // ts/tsx: lint & formatting
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand,'prettier --write'],
+  // ts/tsx: lint & formatting
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand, 'prettier --write'],
   // 타입 체크: 변경사항이 있을 때 프로젝트 전체 검사
-  '**/*.{ts,tsx}': [() => 'pnpm check-types']
+  '**/*.{ts,tsx}': [() => 'pnpm check-types'],
 };
 
 export default config;
