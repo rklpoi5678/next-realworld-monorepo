ab03b7fe425f77c3c670e127cc88864b894e12c1
rklpoi5678 <rklpoi5678@naver.com>
2026-01-09 00:15:56 +0900

    fix: 스키마 경로에 .prisma확장자 추가 필요

--- apps/shadcn-taxonomy/prisma.config.ts
+++ apps/shadcn-taxonomy/prisma.config.ts
@@ -2,7 +2,7 @@
 import {defineConfig, env} from 'prisma/config'
 
 export default defineConfig({
-    schema: "./prisma/schema",
+    schema: "./prisma/schema.prisma",
     datasource: {
         url: env("DATABASE_URL")
     }
