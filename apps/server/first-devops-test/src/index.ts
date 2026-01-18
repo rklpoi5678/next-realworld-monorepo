a967c35f92055aceea91bbdf92cfb1398add55b5
rklpoi5678 <rklpoi5678@naver.com>
2026-01-18 21:18:38 +0900

    chore: 도커설정  fe,be 프로젝트

--- apps/server/first-devops-test/src/index.ts
+++ apps/server/first-devops-test/src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 import cors from 'cors'
 
 const app = express()
@@ -6,11 +6,11 @@ const app = express()
 app.use(cors({ origin: 'http://localhost:3000' })) // FE주소 ( 추후 환경변수 처리 해야합니다.)
 
 /** 헬스체크 엔드포인트*/
-app.get('/health', (req, res) => {
+app.get('/api/health', (req: Request, res: Response) => {
   res.status(200).json({ status: 'ok', uptime: process.uptime() })
 })
 
-app.get('/api/hello', (req, res) => {
+app.get('/api/hello', (req: Request, res: Response) => {
   res.json({ message: 'Hello from Express!' })
 })
 
